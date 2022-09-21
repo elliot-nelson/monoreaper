e8a6cf141f2d58f4f86a3b780fedfc359f952a2a
Elliot Nelson <nelson.work@gmail.com>
2022-09-21 08:31:12 -0400

    Add some bad art representing monorepos

--- src/pages/index.js
+++ src/pages/index.js
@@ -8,6 +8,10 @@ import HomepageFeatures from '@site/src/components/HomepageFeatures';
 import styles from './index.module.css';
 
 import XYZ from '@site/docs/monorepo/monorepo.png';
+import Pillar1 from '@site/docs/monorepo/pillar1.png';
+import Pillar2 from '@site/docs/monorepo/pillar2.png';
+import Pillar3 from '@site/docs/monorepo/pillar3.png';
+import AllPillar from '@site/docs/monorepo/3pillars.png';
 
 
 function HomepageHeader() {
@@ -17,7 +21,7 @@ function HomepageHeader() {
       <div className="container">
         <h1 className="hero__title"> monorepo </h1>
         <p className="hero__subtitle"> monorepos so great </p>
-        <img src={XYZ} width="600px" />
+        <img src={AllPillar} />
       </div>
     </header>
   );
@@ -36,6 +40,15 @@ export default function Home() {
             <br></br>
             <h1>Get more done, faster, with fewer people.</h1>
             <h2>How do you monorepo?</h2>
+            <p>First, you _adopt_ the three pillars of a monorepo: collocation, coherence, and standardization.</p>
+            <p>Then, you _reap the benefits_ of the monorepo developer experience: </p>
+            <ul>
+              <li>Code reuse (the right way)</li>
+              <li>Reduced maintenance cost</li>
+              <li>Tight developer feedback loops</li>
+              <li>Get more done, faster, with fewer people</li>
+            </ul>
+            <p>Three pillars of a monorepo: collocation, coherence, standardization</p>
             <h3>1. Collocated code</h3>
             <ul>
               <li>All of the projects in the monorepo are located in the same repo.</li>
@@ -62,6 +75,40 @@ export default function Home() {
               <li>Reorganize projects and make breaking changes quickly</li>
               <li>Probably more?</li>
             </ul>
+          </div>
+        </section>
+        <p>design notes: use parchment colors, like https://outerbounds.com/ </p>
+        <section className="monorepostuff">
+          <div className="container">
+            <div className="row">
+              <div className={clsx('col col--4')}>
+                <div className="text--center">
+                  <img src={Pillar1}></img>
+                </div>
+                <div className="text--center padding-horiz--md">
+                  <h3>Collocation</h3>
+                  <p>Code is stored together, in a predictable and discoverable way.</p>
+                </div>
+              </div>
+              <div className={clsx('col col--4')}>
+                <div className="text--center">
+                  <img src={Pillar2}></img>
+                </div>
+                <div className="text--center padding-horiz--md">
+                  <h3>Coherence</h3>
+                  <p>Developers work from a single branch and all projects live in that branch.</p>
+                </div>
+              </div>
+              <div className={clsx('col col--4')}>
+                <div className="text--center">
+                  <img src={Pillar3}></img>
+                </div>
+                <div className="text--center padding-horiz--md">
+                  <h3>Standardization</h3>
+                  <p>Standardized tooling across all projects in the monorepo.</p>
+                </div>
+              </div>
+            </div>
           </div>
         </section>
         <HomepageFeatures />
